1efb26620cbf7eb0a2476adb8f173568a66c751f
Ayman097 <engaymanfarag1997@gmail.com>
2024-01-03 15:55:29 +0200

    Adding full-text search to the blog & replace sqlite by postgresql

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,10 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
+from django.contrib.postgres.search import SearchVector
 from django.db.models import Count
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 
 # Create your views here.
@@ -131,3 +132,22 @@ def post_comment(request, post_id):
     )
 
 
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                            search=SearchVector('title', 'body'),).filter(search=query)
+            
+    return render(request,
+                'blog/post/search.html',
+                {'form': form,
+                'query': query,
+                'results': results})
