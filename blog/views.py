304162f9a50ccecd17821c90cce8db5cf397c20c
Ayman097 <engaymanfarag1997@gmail.com>
2024-01-03 17:08:15 +0200

    Adding another search approache to search by similarity, this feature need to CREATE EXTENSION pg_trgm;

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,9 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from django.db.models import Count
-from .models import Post, Comment
+from .models import Post
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 
@@ -143,8 +143,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            # search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            # search_query = SearchQuery(query)
             results = Post.published.annotate(
-                            search=SearchVector('title', 'body'),).filter(search=query)
+                similarity=TrigramSimilarity('title', query),
+                ).filter(similarity__gt=0.1).order_by('-similarity')
             
     return render(request,
                 'blog/post/search.html',
